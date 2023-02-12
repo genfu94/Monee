a68b0a9aa192906b2e225176ecfc00cbe1bde170
genfu94 <giuseppe.gelfusa@hotmail.it>
2023-02-12 20:08:41 +0100

    Started to completely dockerize keycloak

--- keycloak/test_keycloak.py
+++ keycloak/test_keycloak.py
@@ -2,41 +2,36 @@ from keycloak import KeycloakAdmin
 from keycloak.exceptions import KeycloakGetError
 
 
-class KeycloakConfigurator:
-    def __init__(self):
-        pass
-
-    def connect(self):
-        self.keycloak_admin = KeycloakAdmin(server_url="http://localhost:8080",
+if __name__ == "__main__":
+    keycloak_admin = KeycloakAdmin(server_url="http://localhost:8080",
                                     username='admin',
                                     password='admin',
                                     realm_name="master",
                                     verify=True)
-    
-    def _create_realm(self):
-        self.keycloak_admin.create_realm({
+    keycloak_admin.create_realm({
             "realm": "budget_app",
             "enabled": True
         }, skip_exists=True)
+    keycloak_admin.realm_name = "budget_app"
 
-    def _create_client(self):
-        self.keycloak_admin.realm_name = "budget_app"
-        self.keycloak_admin.create_client({
+    keycloak_admin.create_client({
             "name": "budget_app",
             "clientId": "budget_app",
-            "rootUrl": "http://localhost:3000"
+            "rootUrl": "http://localhost:3000",
+            "redirectUris": ["http://localhost:3000/*"],
+            "webOrigins": ["http://localhost:3000"],
+            "adminUrl": "http://localhost:3000",
+            "standardFlowEnabled": True,
+            "enabled": True,
+            "publicClient": True
         }, skip_exists=True)
 
-    def set_configuration(self):
-        self._create_realm()
-        self._create_client()
-
-
-if __name__ == "__main__":
-    keycloak_configurator = KeycloakConfigurator()
-    keycloak_configurator.connect()
-    keycloak_configurator.set_configuration()
-    
-
-
-
+    keycloak_admin.create_user({
+        "username": "user",
+        "enabled": True,
+        "credentials": [{
+            "temporary": False,
+            "type": "Password",
+            "value": "user"
+        }]
+    })
