b47cb3ebc00334d397faa92cfb8c980df3b82673
Giuseppe Gelfusa <giuseppe.gelfusa@hotmail.it>
2023-04-06 07:35:29 +0200

    Styling transaction list item

--- frontend/src/components/RecordList/RecordList.js
+++ frontend/src/components/RecordList/RecordList.js
@@ -7,34 +7,40 @@ export default function RecordList() {
   return (
     <div className="record-entry">
       <div className="record-selector">
-        <input style={{margin: "5px"}} type="checkbox" />
+        <input style={{ margin: "5px" }} type="checkbox" />
       </div>
       <div className="record-info-container">
-        <div className="record-info-category">
-          <span className="circle">
-            <HiOutlineLightBulb style={{ color: "#fff" }} />
-          </span>
-          <span
-            style={{
-              fontWeight: 800,
-              fontSize: "0.8em",
-              fontFamily: "Montserrat",
-            }}
-          >
-            &nbsp;Energy, utilities
-          </span>
+        <div className="record-info">
+          <div className="record-info-category">
+            <span className="circle">
+              <HiOutlineLightBulb style={{ color: "#fff" }} />
+            </span>
+            <span
+              style={{
+                fontWeight: 800,
+                fontSize: "0.8em",
+                fontFamily: "Montserrat",
+              }}
+            >
+              &nbsp;Energy, utilities
+            </span>
+          </div>
+          <div className="record-info-account">
+            <BsCircleFill
+              style={{ fontSize: "0.5em", color: "rgb(255, 160, 0)" }}
+            />
+            &nbsp; N26 - Conto corrente principale
+          </div>
+          <div className="record-info-entity">IT52V0200874130000400611684</div>
+          <div className="record-info-description">Luce & Gas 02/23</div>
         </div>
-        <div className="record-info-account">
-          <BsCircleFill
-            style={{ fontSize: "0.5em", color: "rgb(255, 160, 0)" }}
-          />
-          &nbsp; N26 - Conto corrente principale
-        </div>
-        <div className="record-info-entity">
-          IT52V0200874130000400611684
-        </div>
-        <div className="record-info-description">
-          Luce & Gas 02/23
+        <div className="transaction-amount-section">
+          <div className="target-currency-transaction-amount">
+            - €42.15
+          </div>
+          <div className="original-currency-transaction-amount">
+            - €42.15
+          </div>
         </div>
       </div>
     </div>
