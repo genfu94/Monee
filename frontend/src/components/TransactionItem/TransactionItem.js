ecfe945825a624e09d685c17f6a851f706e088a1
Giuseppe Gelfusa <giuseppe.gelfusa@hotmail.it>
2023-04-12 09:33:19 +0200

    Refactored transaction item out of TransactionList component

---
+++ frontend/src/components/TransactionItem/TransactionItem.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./style.css";
+import { HiOutlineLightBulb } from "react-icons/hi";
+import { BsCircleFill } from "react-icons/bs";
+
+
+export default function TransactionItem(transaction) {
+  return (
+    <div className="transaction">
+      <div className="transaction-select-box">
+        <input type="checkbox" />
+      </div>
+      <div className="transaction-info-container">
+        <div className="transaction-info">
+          <div className="transaction-info-category">
+            <span className="circle">
+              <HiOutlineLightBulb style={{ color: "#fff" }} />
+            </span>
+            <span
+              style={{
+                fontWeight: 800,
+                fontSize: "0.8em",
+                fontFamily: "Montserrat",
+              }}
+            >
+              &nbsp;Energy, utilities
+            </span>
+          </div>
+          <div className="transaction-info-account">
+            <BsCircleFill
+              style={{ fontSize: "0.5em", color: "rgb(255, 160, 0)" }}
+            />
+            &nbsp; N26 - Conto Corrente Principale
+          </div>
+          <div className="transaction-info-entity">
+            {transaction.origin}
+          </div>
+          <div className="transaction-info-description">{transaction.text}</div>
+        </div>
+        <div className="transaction-amount-section">
+          <div className={`target-currency-transaction-amount ${transaction.transaction_amount.amount > 0 ? "transaction-income":""}`}>{transaction.transaction_amount.currency}&nbsp;{transaction.transaction_amount.amount}</div>
+          <div className="original-currency-transaction-amount">{transaction.transaction_amount.currency}&nbsp;{transaction.transaction_amount.amount}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/components/TransactionList/TransactionList.js
+++ frontend/src/components/TransactionList/TransactionList.js
@@ -1,55 +1,12 @@
 import React from "react";
 import "./style.css";
-import { HiOutlineLightBulb } from "react-icons/hi";
 import { AiFillEye } from "react-icons/ai";
 import Checkbox from "@mui/material/Checkbox";
-import { BsCircleFill } from "react-icons/bs";
+import TransactionItem from "../TransactionItem/TransactionItem.js"
 
 export default class TransactionList extends React.Component {
   constructor(props) {
     super(props);
-  }
-
-  #renderTransactionItem(transaction) {
-    return (
-      <div className="transaction">
-        <div className="transaction-select-box">
-          <input type="checkbox" />
-        </div>
-        <div className="transaction-info-container">
-          <div className="transaction-info">
-            <div className="transaction-info-category">
-              <span className="circle">
-                <HiOutlineLightBulb style={{ color: "#fff" }} />
-              </span>
-              <span
-                style={{
-                  fontWeight: 800,
-                  fontSize: "0.8em",
-                  fontFamily: "Montserrat",
-                }}
-              >
-                &nbsp;Energy, utilities
-              </span>
-            </div>
-            <div className="transaction-info-account">
-              <BsCircleFill
-                style={{ fontSize: "0.5em", color: "rgb(255, 160, 0)" }}
-              />
-              &nbsp; N26 - Conto Corrente Principale
-            </div>
-            <div className="transaction-info-entity">
-              {transaction.origin}
-            </div>
-            <div className="transaction-info-description">{transaction.text}</div>
-          </div>
-          <div className="transaction-amount-section">
-            <div className={`target-currency-transaction-amount ${transaction.transaction_amount.amount > 0 ? "transaction-income":""}`}>{transaction.transaction_amount.currency}&nbsp;{transaction.transaction_amount.amount}</div>
-            <div className="original-currency-transaction-amount">{transaction.transaction_amount.currency}&nbsp;{transaction.transaction_amount.amount}</div>
-          </div>
-        </div>
-      </div>
-    );
   }
 
   render() {
@@ -66,7 +23,7 @@ export default class TransactionList extends React.Component {
         </ul>
         <div>
           {
-            Object.keys(this.props.transactions).map((transaction_id) => this.#renderTransactionItem(this.props.transactions[transaction_id]))
+            Object.keys(this.props.transactions).map((transaction_id) => TransactionItem(this.props.transactions[transaction_id]))
           }
         </div>
       </div>
