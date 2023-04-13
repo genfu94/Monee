1c420d71e37d409a07a98ccaa6d4b518e9ed4711
Giuseppe Gelfusa <giuseppe.gelfusa@hotmail.it>
2023-04-13 09:17:41 +0200

    Refactoring and restyling transaction list

--- frontend/src/components/TransactionList/TransactionList.js
+++ frontend/src/components/TransactionList/TransactionList.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./TransactionList.style.css";
 import { AiFillEye } from "react-icons/ai";
 import Checkbox from "@mui/material/Checkbox";
-import TransactionItem from "./TransactionItem/TransactionItem.js"
+import TransactionItem from "./TransactionItem/TransactionItem.js";
 
 export default class TransactionList extends React.Component {
   constructor(props) {
@@ -12,7 +12,7 @@ export default class TransactionList extends React.Component {
   render() {
     return (
       <div className="transaction-view-container elevation-1">
-        <ul className="transaction-selector">
+        <ul className="flex-container-vertical transaction-selector">
           <li>
             <Checkbox
               icon={<AiFillEye style={{ color: "#292929" }} />}
@@ -21,10 +21,12 @@ export default class TransactionList extends React.Component {
             N26 - Conto corrente principale
           </li>
         </ul>
-        <div class="flex-container-vertical transaction-list">
-          {
-            Object.keys(this.props.transactions).map((transaction_id) => TransactionItem(this.props.transactions[transaction_id]))
-          }
+        <div className="transaction-list-container">
+          <div className="flex-container-vertical transaction-list">
+            {Object.keys(this.props.transactions).map((transaction_id) =>
+              TransactionItem(this.props.transactions[transaction_id])
+            )}
+          </div>
         </div>
       </div>
     );
