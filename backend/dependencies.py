03313a8e5c89301f7edca4d1a1c8ce1a0734161f
Giuseppe Gelfusa <giuseppe.gelfusa@hotmail.it>
2023-04-14 17:22:13 +0200

    Separate nordigen secret id and key in a separate config file

--- backend/dependencies.py
+++ backend/dependencies.py
@@ -10,13 +10,16 @@ bank_sync_client:BankSyncClient = None
 def initialize_bank_sync_client():
     global bank_sync_client
 
+    generic_config = configparser.ConfigParser()
+    generic_config.read('generic_config.ini')
+
+    account_db_client = MongoAccountDatabaseClient(generic_config['DATABASE']['DBConnectionString'])
+
     config = configparser.ConfigParser()
     config.read('config.ini')
 
-    account_db_client = MongoAccountDatabaseClient(config['DATABASE']['DBConnectionString'])
-
     bank_sync_client = NordigenBankSyncClient(
-        APICredentials(config['DEFAULT']['NordigenSecretID'], config['DEFAULT']['NordigenSecretKey']),
+        APICredentials(config['NORDIGEN']['NordigenSecretID'], config['NORDIGEN']['NordigenSecretKey']),
         account_db_client
     )
 
