46e0eff5922e3657afca25fc760bc51b63f81356
Giuseppe Gelfusa <giuseppe.gelfusa@hotmail.it>
2023-04-17 15:47:55 +0200

    Refactored transaction list

--- frontend/src/components/TransactionList/TransactionItem/TransactionItem.js
+++ frontend/src/components/TransactionList/TransactionItem/TransactionItem.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./TransactionItem.style.css";
 import { HiOutlineLightBulb } from "react-icons/hi";
 import { BsCircleFill } from "react-icons/bs";
-import Modal from '@mui/material/Modal';
-import Box from '@mui/material/Box';
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
 
 export default function TransactionItem(props) {
   const [open, setOpen] = React.useState(false);
@@ -11,79 +11,73 @@ export default function TransactionItem(props) {
   const handleClose = () => setOpen(false);
 
   const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
     width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
+    bgcolor: "background.paper",
+    border: "2px solid #000",
     boxShadow: 24,
     p: 4,
   };
 
   let transaction = props.transaction;
   return (
-    <div style={{display: "flex", width:"100%", justifyContent: "center"}}>
-      <div
-        className="flex-centered selectable-item transaction"
-        onClick={handleOpen}
-      >
-        <div className="flex-centered">
-          <input type="checkbox" />
+    <div
+      className="flex-centered selectable-item transaction"
+      onClick={handleOpen}
+    >
+      <input type="checkbox" />
+      <div className="flex-centered transaction-info">
+        <div className="flex-centered transaction-info-category">
+          <span className="flex-centered circle">
+            <HiOutlineLightBulb style={{ color: "#fff" }} />
+          </span>
+          <span
+            style={{
+              fontWeight: 800,
+              fontSize: "0.8em",
+              fontFamily: "Montserrat",
+            }}
+          >
+            &nbsp;Energy, utilities
+          </span>
         </div>
-        <div className="flex-centered transaction-info-container">
-          <div className="flex-centered transaction-info">
-            <div className="flex-centered transaction-info-category">
-              <span className="flex-centered circle">
-                <HiOutlineLightBulb style={{ color: "#fff" }} />
-              </span>
-              <span
-                style={{
-                  fontWeight: 800,
-                  fontSize: "0.8em",
-                  fontFamily: "Montserrat",
-                }}
-              >
-                &nbsp;Energy, utilities
-              </span>
-            </div>
-            <div className="flex-centered transaction-info-account">
-              <BsCircleFill
-                style={{ fontSize: "0.5em", color: "rgb(255, 160, 0)" }}
-              />
-              &nbsp; N26 - Conto Corrente Principale
-            </div>
-            <div className="flex-centered transaction-info-entity">
-              {transaction.origin}
-            </div>
-            <div className="flex-centered transaction-info-description">
-              {transaction.text}
-            </div>
-          </div>
-          <div className="flex-centered-vertical transaction-amount-section">
-            <div
-              className={`${
-                transaction.transaction_amount.amount > 0
-                  ? "transaction-income"
-                  : "target-currency-transaction-amount"
-              }`}
-            >
-              {transaction.transaction_amount.currency}&nbsp;
-              {transaction.transaction_amount.amount}
-            </div>
-            <div className="original-currency-transaction-amount">
-              {transaction.transaction_amount.currency}&nbsp;
-              {transaction.transaction_amount.amount}
-            </div>
-          </div>
+        <div className="flex-centered transaction-info-account">
+          <BsCircleFill
+            style={{ fontSize: "0.5em", color: "rgb(255, 160, 0)" }}
+          />
+          &nbsp; N26 - Conto Corrente Principale
+        </div>
+        <div className="flex-centered transaction-info-entity">
+          {transaction.origin}
+        </div>
+        <div className="flex-centered transaction-info-description">
+          {transaction.text}
         </div>
       </div>
 
-      <Modal disableAutoFocus={true} open={open} onClose={handleClose}>
-        <Box sx={style}>
+      <div className="flex-centered-vertical transaction-amount-section">
+        <div
+          className={`${
+            transaction.transaction_amount.amount > 0
+              ? "transaction-income"
+              : "target-currency-transaction-amount"
+          }`}
+        >
+          {transaction.transaction_amount.currency}&nbsp;
           {transaction.transaction_amount.amount}
-        </Box>
+        </div>
+        <div className="original-currency-transaction-amount">
+          {transaction.transaction_amount.currency}&nbsp;
+          {transaction.transaction_amount.amount}
+        </div>
+        
+      </div>
+
+      <Modal disableAutoFocus={true} open={open} onClose={handleClose}>
+        <Box sx={style}>{transaction.transaction_amount.amount}</Box>
       </Modal>
     </div>
   );
--- frontend/src/components/TransactionList/TransactionList.js
+++ frontend/src/components/TransactionList/TransactionList.js
@@ -1,11 +1,8 @@
 import React from "react";
 import "./TransactionList.style.css";
-import { AiFillEye } from "react-icons/ai";
-import Checkbox from "@mui/material/Checkbox";
 import TransactionItem from "./TransactionItem/TransactionItem.js";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
 import ListSubheader from "@mui/material/ListSubheader";
 
 export default class TransactionList extends React.Component {
@@ -15,35 +12,22 @@ export default class TransactionList extends React.Component {
 
   render() {
     return (
-      <div className="transaction-list-container">
-        <List
-          sx={{
-            display: "flex",
-            width: "100%",
-            justifyContent: "center",
-            flexDirection: "column"
-          }}
-        >
-          {Object.keys(this.props.transactions).map((sectionId) => (
-            <li key={`section-${sectionId}`}>
-              <ul style={{ margin: "0", padding: "0" }}>
-                <ListSubheader style={{ backgroundColor: "#eff0f2" }}>
-                  <div style={{ fontSize: "12px", fontWeight: "900" }}>
-                    {sectionId}
-                  </div>
-                </ListSubheader>
-                {this.props.transactions[sectionId].map((item) => {
-                  return (
-                    <ListItem key={`item-${item.transaction_id}`}>
-                      <TransactionItem transaction={item} />
-                    </ListItem>
-                  );
-                })}
-              </ul>
-            </li>
-          ))}
-        </List>
-      </div>
+      <List>
+        {Object.keys(this.props.transactions).map((sectionId) => (
+          <li key={`section-${sectionId}`}>
+              <ListSubheader style={{ backgroundColor: "#eff0f2", fontSize: "12px", fontWeight: "900" }}>
+                  {sectionId}
+              </ListSubheader>
+              {this.props.transactions[sectionId].map((item) => {
+                return (
+                  <ListItem key={`item-${item.transaction_id}`}>
+                    <TransactionItem transaction={item} />
+                  </ListItem>
+                );
+              })}
+          </li>
+        ))}
+      </List>
     );
   }
 }
