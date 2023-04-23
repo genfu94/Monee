52f652a1c60f4ed6634995ba39bafde11da2ac93
Giuseppe Gelfusa <giuseppe.gelfusa@hotmail.it>
2023-04-23 08:26:55 +0200

    Styling nested selector

--- frontend/src/components/TransactionForm/NestedSelector/NestedSelector.js
+++ frontend/src/components/TransactionForm/NestedSelector/NestedSelector.js
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
 import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
 import { GoTriangleDown } from "react-icons/go";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
+import { IoChevronBackOutline } from "react-icons/io5";
 
 import "./NestedSelector.style.css";
 
 export default function NestedSelector() {
   const data = [
     {
-      label: <div>ASD</div>,
+      value: "Food & Drinks",
+      label: <div>Food & Drinks</div>,
       submenu: [
         {
-          value: 0,
-          label: <div>LOL</div>,
-          submenu: []
+          value: "Bar & Cafè",
+          label: <div>Bar & Cafè</div>,
+          submenu: [],
         },
       ],
     },
@@ -22,35 +25,50 @@ export default function NestedSelector() {
   const [open, setOpen] = useState(false);
   const [level, setLevel] = useState([data]);
   const [value, setValue] = useState(null);
+  const [parent, setParent] = useState("");
   const switchOpen = () => setOpen(!open);
   const addLevel = (v) => {
     if (v.submenu == null || v.submenu.length == 0) {
-      console.log("XDDDD")
       setOpen(false);
       setValue(v.label);
     } else {
-      setLevel((oldLevel) => [v.submenu, ...oldLevel])
+      setParent(v.value);
+      setLevel((oldLevel) => [v.submenu, ...oldLevel]);
     }
-  }
-  const goBackLevel = () => setLevel(level => level.filter((l,i)=>(i != 0)))
+  };
+  const goBackLevel = () => setLevel((level) => level.filter((l, i) => i != 0));
 
   return (
     <div className="nested-select-container">
       <Button
         onClick={switchOpen}
         variant="outlined"
-        sx={{ width: "100%", height: "50px", borderColor: "inherit" }}
+        sx={{ width: "100%", height: "50px", borderColor: "inherit", textTransform: "none" }}
       >
         {value}
         <GoTriangleDown style={{ marginLeft: "auto" }} />
       </Button>
       {open && (
         <div className="options">
-          {level.length > 1 && <Button onClick={goBackLevel} variant="outlined">Go Back</Button>}
+          {level.length > 1 && (
+            <div>
+              <IconButton
+                size="small"
+                onClick={goBackLevel}
+              >
+                <IoChevronBackOutline />
+              </IconButton>
+              <span>{parent}</span>
+            </div>
+          )}
           <List>
             {level[0].map((item, idx) => {
               return (
-                <ListItem onClick={() => addLevel(item)} key={idx} className="select-item">
+                <ListItem
+                  onClick={() => addLevel(item)}
+                  key={idx}
+                  className="select-item"
+                >
                   {item.label}
                 </ListItem>
               );
