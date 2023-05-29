f60d7f291f9d1614a2c11ec51d382e4e7a38d8d2
Giuseppe Gelfusa <giuseppe.gelfusa@hotmail.it>
2023-05-29 18:24:14 +0200

    Naive transaction classification pipeline implementation

--- transaction_classification/transaction_classification.py
+++ transaction_classification/transaction_classification.py
@@ -1,10 +1,84 @@
 from sklearn.feature_extraction.text import CountVectorizer
-import nltk
-nltk.download('stopwords')
-from nltk.corpus import stopwords
+import pandas as pd
 import numpy as np
+import re
+from sklearn import svm
+from joblib import dump
+import categories
+from typing import List
+from functools import reduce
+from sklearn.pipeline import Pipeline
 
-count_vec = CountVectorizer()
-bow = count_vec.fit_transform(["BBVA.IT", "ADR MOBILITY SRL", "Cena Livella", "Cena Indiano", "Bowling"])
-#bow = np.array(bow.todense())
-print(bow)
+
+class WalletCsvFileIngestion:
+    def initialize(self, filepath: str):
+        self.filepath = filepath
+        return self
+
+    def execute(self) -> pd.DataFrame:
+        transaction_data = pd.read_csv(self.filepath, sep=';')
+        return transaction_data[['category', 'type', 'note']]
+
+
+class WalletDataFramePreprocess:
+    def initialize(self, wallet_transaction_df: pd.DataFrame):
+        self.wallet_transaction_df = wallet_transaction_df
+        return self
+    
+    def execute(self) -> list:
+        self.wallet_transaction_df.category = self.wallet_transaction_df.category.map(lambda x: categories.convert_wallet_categories(x).value)
+        self.wallet_transaction_df = self.wallet_transaction_df.query("type != 'Expense' and type != 'Income' ")
+        self.wallet_transaction_df.note = self.wallet_transaction_df.note.fillna('')
+
+        X = self.wallet_transaction_df.note.to_numpy()
+        y = self.wallet_transaction_df.category.to_numpy()
+        return X, y
+
+
+class TransactionTextPreprocessing:
+    def fit(self, X, y):
+        return self
+    
+    def transform(self, X):
+        return list(map(lambda x: re.sub(r"[^a-zA-Z0-9 ]", " ", x), X))
+
+
+class TransactionTextToBOW:
+    def __init__(self):
+        self.count_vec = CountVectorizer()
+
+    def fit(self, X, y):        
+        self.count_vec.fit(X)
+        return self
+    
+    def transform(self, X):
+        bow = self.count_vec.transform(X)
+        X = np.array(bow.todense())
+        return X
+
+class InferencePipeline:
+    def __init__(self, data_preparations_steps: List, inference_steps: List):
+        self.data_preparation_steps = data_preparations_steps
+        self.inference_steps = inference_steps
+    
+    def fit(self, filepath: str):
+        X, y = reduce(lambda f, g: g.initialize(f).execute(), self.data_preparation_steps, filepath)
+        self.pipe = Pipeline(self.inference_steps)
+        self.pipe.fit(X, y)
+    
+    def predict(self, transaction_text: str):
+        return self.pipe.predict([transaction_text])[0]
+
+
+if __name__ == '__main__':
+    pipe = InferencePipeline([
+        WalletCsvFileIngestion(),
+        WalletDataFramePreprocess()
+    ], [
+        ('text_clean', TransactionTextPreprocessing()),
+        ('bow', TransactionTextToBOW()),
+        ('clf', svm.SVC(kernel='linear', C=1))
+    ])
+    pipe.fit('./dataset.csv')
+    dump(pipe, 'transaction_classifier.joblib')
+
