a6b3c8bf8c0a04f1c042e54eec4dae3b67964183
Giuseppe Gelfusa <giuseppe.gelfusa@hotmail.it>
2023-05-30 16:36:42 +0200

    Improved transaction categorization model

--- backend/transaction_classification.py
+++ backend/transaction_classification.py
@@ -8,30 +8,50 @@ import categories
 from typing import List
 from functools import reduce
 from sklearn.pipeline import Pipeline
-
+#from transformers import BertTokenizer, TFBertModel
+from sklearn.model_selection import cross_validate
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import make_scorer, f1_score
 
 class WalletCsvFileIngestion:
-    def initialize(self, filepath: str):
+    def __init__(self, filepath: str):
         self.filepath = filepath
+    
+    def initialize(self, transaction_data):
+        self.transaction_data = transaction_data
         return self
 
     def execute(self) -> pd.DataFrame:
-        transaction_data = pd.read_csv(self.filepath, sep=';')
-        return transaction_data[['category', 'type', 'note']]
+        new_transaction_data = pd.read_csv(self.filepath, sep=';')
+        new_transaction_data = new_transaction_data.query("type != 'Expense' and type != 'Income' ")
+        new_transaction_data.note = new_transaction_data.note.fillna('')
+        new_transaction_data.category = new_transaction_data.category.map(lambda x: categories.convert_wallet_categories(x).value)
+        return pd.concat([self.transaction_data, new_transaction_data[['category', 'note']]])
+
+
+class UnicreditCsvFileIngestion:
+    def __init__(self, filepath: str):
+        self.filepath = filepath
+    
+    def initialize(self, transaction_data):
+        self.transaction_data = transaction_data
+        return self
+
+    def execute(self) -> pd.DataFrame:
+        new_transaction_data = pd.read_csv(self.filepath, sep=',')
+
+        return pd.concat([self.transaction_data, new_transaction_data[['category', 'note']]])
 
 
 class WalletDataFramePreprocess:
-    def initialize(self, wallet_transaction_df: pd.DataFrame):
-        self.wallet_transaction_df = wallet_transaction_df
+    def initialize(self, transaction_df: pd.DataFrame):
+        self.transaction_df = transaction_df
         return self
     
     def execute(self) -> list:
-        self.wallet_transaction_df.category = self.wallet_transaction_df.category.map(lambda x: categories.convert_wallet_categories(x).value)
-        self.wallet_transaction_df = self.wallet_transaction_df.query("type != 'Expense' and type != 'Income' ")
-        self.wallet_transaction_df.note = self.wallet_transaction_df.note.fillna('')
-
-        X = self.wallet_transaction_df.note.to_numpy()
-        y = self.wallet_transaction_df.category.to_numpy()
+        X = self.transaction_df.note.to_numpy()
+        y = self.transaction_df.category.to_numpy()
         return X, y
 
 
@@ -43,9 +63,26 @@ class TransactionTextPreprocessing:
         return list(map(lambda x: re.sub(r"[^a-zA-Z0-9 ]", " ", x), X))
 
 
+# class BertEmbedding:
+#     def __init__(self):
+#         self.tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-uncased')
+#         self.model = TFBertModel.from_pretrained("bert-base-multilingual-uncased")
+
+#     def fit(self, X, y):
+#         return self
+
+#     def _encode(self, text):
+#         encoded_input = self.tokenizer(text, return_tensors='tf')
+#         output = self.model(encoded_input)
+#         return output.pooler_output.numpy()[0]
+
+#     def transform(self, X):
+#         return list(map(self._encode, X))
+        
+
 class TransactionTextToBOW:
     def __init__(self):
-        self.count_vec = CountVectorizer()
+        self.count_vec = CountVectorizer(max_df=0.1)
 
     def fit(self, X, y):        
         self.count_vec.fit(X)
@@ -61,9 +98,12 @@ class InferencePipeline:
         self.data_preparation_steps = data_preparations_steps
         self.inference_steps = inference_steps
     
-    def fit(self, filepath: str):
-        X, y = reduce(lambda f, g: g.initialize(f).execute(), self.data_preparation_steps, filepath)
+    def fit(self):
+        scoring = {'f1_score' : make_scorer(f1_score, average='macro')}
+        
+        X, y = reduce(lambda f, g: g.initialize(f).execute(), self.data_preparation_steps, pd.DataFrame(columns=['category', 'note']))
         self.pipe = Pipeline(self.inference_steps)
+        print(cross_validate(self.pipe, X, y, cv=5, scoring=scoring))
         self.pipe.fit(X, y)
     
     def predict(self, transaction_text: str):
--- transaction_classification/main.py
+++ transaction_classification/main.py
@@ -1,16 +1,25 @@
-from transaction_classification import InferencePipeline, WalletCsvFileIngestion, WalletDataFramePreprocess, TransactionTextPreprocessing, TransactionTextToBOW
+from transaction_classification import InferencePipeline, WalletCsvFileIngestion, WalletDataFramePreprocess, TransactionTextPreprocessing, TransactionTextToBOW, BertEmbedding, UnicreditCsvFileIngestion
 from sklearn import svm
 from joblib import dump
+from sklearn.naive_bayes import MultinomialNB
+import pandas as pd
+from  sklearn.ensemble import GradientBoostingClassifier
+
+#def add_category_to_dataframe(transaction_classifier, df: pd.DataFrame, text_column_idx: int):
+#    df['category'] = df.apply(lambda row: transaction_classifier.predict(row[text_column_idx]), axis=1)
 
 
 if __name__ == '__main__':
     pipe = InferencePipeline([
-        WalletCsvFileIngestion(),
+        WalletCsvFileIngestion('./data/clean/export_wallet.csv'),
+        UnicreditCsvFileIngestion('./data/clean/export_unicredit.csv'),
         WalletDataFramePreprocess()
     ], [
         ('text_clean', TransactionTextPreprocessing()),
         ('bow', TransactionTextToBOW()),
-        ('clf', svm.SVC(kernel='linear', C=1))
+        #('embedding', BertEmbedding()),
+        #('clf', svm.SVC(kernel='linear', C=1000))
+        ('clf', GradientBoostingClassifier())
     ])
-    pipe.fit('./dataset.csv')
+    pipe.fit()
     dump(pipe, 'transaction_classifier.joblib')
