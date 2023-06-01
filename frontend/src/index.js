8d816a05a86c3278f117e142e99eb19c2eaf5dfe
Giuseppe Gelfusa <giuseppe.gelfusa@hotmail.it>
2023-06-01 14:51:26 +0200

    Removed unused code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,16 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-//import { ReactKeycloakProvider } from '@react-keycloak/web'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(
-//       <ReactKeycloakProvider authClient={keycloak} initOptions={{onLoad: 'login-required'}}>
-//         <BrowserRouter>
-//           <App />
-//         </BrowserRouter>
-//       </ReactKeycloakProvider>
-// );
 
 root.render(
   <BrowserRouter>
