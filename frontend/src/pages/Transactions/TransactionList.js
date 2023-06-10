720029f18f4f4043fff3abb006a790db2b58a373
Giuseppe Gelfusa <giuseppe.gelfusa@hotmail.it>
2023-06-10 17:42:37 +0200

    TransactionList.js: moved groupTransactionsByDate to statistics.js

--- frontend/src/pages/Transactions/TransactionList.js
+++ frontend/src/pages/Transactions/TransactionList.js
@@ -4,34 +4,11 @@ import ListItem from "@mui/material/ListItem";
 import ListSubheader from "@mui/material/ListSubheader";
 import Modal from "@mui/material/Modal";
 import Box from "@mui/material/Box";
+import { groupTransactionsByDate } from "../../utils/statistics.js";
 
-import dayjs from "dayjs";
 import TransactionItem from "./TransactionItem.js";
 import TransactionForm from "../../components/TransactionForm/TransactionForm.js";
 import { modalStyle, listSubheaderStyle } from "./TransactionList.style.js";
-
-function groupTransactionsByDate(accounts, accountFilter, dateRange) {
-  let transactionsGroupedByDate = {};
-  for (const account of accounts) {
-    if (!accountFilter.includes(account.id)) continue;
-
-    for (const transaction of account.transactions) {
-      const dates = Object.keys(transactionsGroupedByDate);
-      const booking_date = dayjs(transaction.booking_date, "YYYY-MM-DD, hh:mm:ss");
-
-      if (!(booking_date >= dateRange[0] && booking_date <= dateRange[1]))
-        continue;
-
-      const format_booking_date = booking_date.format("MMM DD, YYYY");
-      if (!dates.includes(format_booking_date)) {
-        transactionsGroupedByDate[format_booking_date] = [];
-      }
-
-      transactionsGroupedByDate[format_booking_date].push({...transaction, account: account.institution_name + ' - ' + account.name});
-    }
-  }
-  return transactionsGroupedByDate;
-}
 
 function TransactionList({
   accounts,
--- frontend/src/utils/statistics.js
+++ frontend/src/utils/statistics.js
@@ -53,3 +53,26 @@ export function groupExpensesByCategory(accounts) {
 
   return expenseByCategory;
 }
+
+export function groupTransactionsByDate(accounts, accountFilter, dateRange) {
+  let transactionsGroupedByDate = {};
+  for (const account of accounts) {
+    if (!accountFilter.includes(account.id)) continue;
+
+    for (const transaction of account.transactions) {
+      const dates = Object.keys(transactionsGroupedByDate);
+      const booking_date = dayjs(transaction.booking_date, "YYYY-MM-DD, hh:mm:ss");
+
+      if (!(booking_date >= dateRange[0] && booking_date <= dateRange[1]))
+        continue;
+
+      const format_booking_date = booking_date.format("MMM DD, YYYY");
+      if (!dates.includes(format_booking_date)) {
+        transactionsGroupedByDate[format_booking_date] = [];
+      }
+
+      transactionsGroupedByDate[format_booking_date].push({...transaction, account: account.institution_name + ' - ' + account.name});
+    }
+  }
+  return transactionsGroupedByDate;
+}
