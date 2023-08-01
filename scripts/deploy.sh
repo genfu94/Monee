5d5734ee31f1048e49a5f9a79fce1e20c66737dc
Giuseppe Gelfusa <giuseppe.gelfusa@hotmail.it>
2023-08-01 09:14:37 +0200

    Reworking cd pipeline

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ cd /home/ubuntu/budget_app
 set -a
 source .env
 aws ecr get-login-password --region eu-north-1 | sudo docker login --username AWS --password-stdin 071386689683.dkr.ecr.eu-north-1.amazonaws.com
-docker compose -f docker-compose.production.yml down
-docker compose -f docker-compose.production.yml pull
-docker compose -f docker-compose.production.yml up
+sudo docker compose -f docker-compose.production.yml down
+sudo docker compose -f docker-compose.production.yml pull
+sudo docker compose -f docker-compose.production.yml up
