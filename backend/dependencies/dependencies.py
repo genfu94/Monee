a0e3ecc2f5911c958dd0516d7d16455c1074562d
Giuseppe Gelfusa <giuseppe.gelfusa@hotmail.it>
2023-08-05 06:53:08 +0200

    Backend: removed unused code

--- backend/dependencies/dependencies.py
+++ backend/dependencies/dependencies.py
@@ -1,31 +1,22 @@
 from services.bank_connect.types import APICredentials
 from services.bank_connect.implementation.nordigen import NordigenBankSyncClient
 from services.database_crud import (
-    AccountCRUD,
-    BankLinkCRUD,
-    TransactionCRUD,
     MongoAccountCRUD,
     MongoBankLinkCRUD,
     MongoTransactionCRUD,
 )
-from services.bank_connect.bank_connect import BankConnector
 from services.bank_sync import BankSync
 from pymongo import MongoClient
 
 from settings import Settings
 
 settings = Settings()
-
-bank_connector: BankConnector = None
-account_crud: AccountCRUD = None
-bank_link_crud: BankLinkCRUD = None
-transaction_crud: TransactionCRUD = None
 bank_sync: BankSync = None
 mongo_client: MongoClient = None
 
 
 def initialize_bank_sync_client():
-    global bank_sync, bank_connector, account_crud, bank_link_crud, transaction_crud, mongo_client
+    global bank_sync, mongo_client
 
     mongo_client = MongoClient(settings.MONGO_DB_CONNECTION_STRING)["budget_app"]
     account_crud = MongoAccountCRUD(mongo_client)
@@ -46,19 +37,3 @@ def get_mongo_client():
 
 def get_bank_sync():
     return bank_sync
-
-
-def get_bank_connector():
-    return bank_connector
-
-
-def get_account_crud():
-    return account_crud
-
-
-def get_bank_link_crud():
-    return bank_link_crud
-
-
-def get_transaction_crud():
-    return transaction_crud
